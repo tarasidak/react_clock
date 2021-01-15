b1eaee1e5ba86543886be6430608bb66ab6b0d8c
Taras Sidak <tarasidak1990@gmail.com>
2021-01-15 18:27:19 +0200

    Updates

---
+++ precache-manifest.e379d84b9463722cfbf8f96d99415ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831f6c2f7041aa2182cdea67b7809529",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b9af805bfca57eb8456",
+    "url": "./static/css/main.e077f0d5.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9af805bfca57eb8456",
+    "url": "./static/js/main.81eb9dd5.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
